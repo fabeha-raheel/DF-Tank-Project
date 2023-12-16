023f80b92383b9c4bf8157babbcdf13a5b0fc942
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2023-12-16 18:45:09 +0300

    deleted useless files

--- tests/fpga_transmission.py
+++
@@ -1,9 +0,0 @@
-
-
-amplitudes = []
-
-with open(r'tests\fpga_data.txt', 'r') as file:
-    for line in file:
-        line = line.strip()
-        if line.isnumeric():
-            amplitudes.append(int(line))
