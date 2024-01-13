22adc66fb7259230e8672cee53330b4d2d37832e
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2024-01-13 13:00:15 +0300

    ugv_client testing set to False

--- ugv_client.py
+++ ugv_client.py
@@ -6,6 +6,6 @@ websocket_url = "ws://192.168.0.116:9090/"
 
 if __name__ == '__main__':
 
-    df_system = DF_System(test=True, ws_url=websocket_url)
+    df_system = DF_System(test=False, ws_url=websocket_url)
 
     df_system.initialize()
