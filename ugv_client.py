24495ba9d61ddc870c74535c81fdba3f5ecee406
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2024-01-24 16:27:22 +0300

    added ugv_client files for testing

---
+++ ugv_client.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from DF_System import *
+
+# websocket_url = "ws://192.168.0.154:9090/"
+websocket_url = "ws://192.168.0.112:9090/"
+
+if __name__ == '__main__':
+
+    df_system = DF_System(ws_url=websocket_url)
+
+    df_system.initialize()
